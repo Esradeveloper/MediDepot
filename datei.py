dfd17b5b422ce74aec35d74fd20f1df269de2020
gueleresra1 <gueler.esra1@web.de>
2025-06-28 06:31:37 +0200

    Revert "Update datei.py"
    This reverts commit 82347026e5d155459a2e89e50befcf548a45026e.

--- datei.py
+++ datei.py
@@ -15,7 +15,4 @@ li = [[('Latexhandschuhe', 60, 10, 'Packung', 'Labor', '17.06.2025', 'MS'),
             ('Mullkompresse 10x10', 2, 3, 'Packung', 'Labor', '17.06.2025', 'EG'),
             ('Optiskin', 2, 1, 'Stück', 'Labor', '17.06.2025', 'EG'),
             ('Leukase Puder', 1, 1, 'Stück', 'Medikamentenschrank', '17.06.2025', 'EG'),
-            ('Skalpell 15 REF', 3, 2, 'Stück', 'Labor', '17.06.2025', 'EG'),
-            ('Burak', 3, 2, 'Stück', 'Labor', '17.06.2025', 'EG'),
-  
-            ]]
+            ('Skalpell 15 REF', 3, 2, 'Stück', 'Labor', '17.06.2025', 'EG')]]
